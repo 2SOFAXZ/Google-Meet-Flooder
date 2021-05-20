69c836adf426c95b93dccb41e68a8e8f608aa009
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2021-05-19 20:41:17 -0400

    Changed controls & added more info ⭐
    Controls are now Ctrl + Alt + B.
    
    I made it easier to use and added more info in description to make it understandable.

--- bot.js
+++ bot.js
@@ -1,73 +1,86 @@
 // ==UserScript==
-// @name         Google Meet Bot
+// @name         Google Meet Spam Bot
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  Spam any google meet class
-// @author       GSRHackZ
-// @match        *://*/*
-// @icon         https://www.flaticon.com/svg/static/icons/svg/939/939840.svg
+// @version      1.0
+// @description  Spam any google meet with bots, chat spam added!!
+// @author       GSRHackZZ
+// @match        https://meet.google.com/*
+// @icon         https://i.ibb.co/F5BWyjw/fixed-removebg-preview.png
 // @grant        none
-// @license                  MIT
-// @compatible               chrome
-// @compatible               firefox
-// @compatible               opera
-// @compatible               safari
 // ==/UserScript==
 
-if(location.href.includes("https://meet.google.com")){
-    if(!location.href.includes("#BOT")){
-        document.body.addEventListener("keyup",function(evt){
-            if(evt.keyCode==192){
-                let botCount=prompt("How many bots would you like to activate? *Enter in number format only* Recommended : less than 15");
-                let link=prompt("Enter link bots should hijack; *Remember to use an email other than your school email!!*");
-                let phrase=prompt("What would you like the robot to keep spamming in chat?");
-                if(botCount.trim().length==0||link.trim().length==0||phrase.trim().length==0){
-                    alert("something is missing, please try again...🤔")
-                }
-                else{
-                    let ask=confirm(`Please confirm by clicking ok...\nbotCount = ${botCount}\nlink = ${link}\nSpam Msg = ${phrase}`);
-                    if(ask){
-                        localStorage.setItem("spamMsg",phrase);
-                        for(let i=0;i<botCount;i++){
-                            window.open(link+"#BOT"+Number(i+1));
-                        }
+window.addEventListener("keyup",function(evt){
+    let dataSet = {"link":"","bots":"","spam":"Inactive","phrase":"none"}
+    if(evt.ctrlKey&&evt.altKey&&evt.keyCode==66){
+        dataSet.link=location.href;
+        dataSet.bots = prompt(`How many bots would you like to spam ${dataSet.link}? Recommended Max : 15 or your browser may crash ☠`)
+        if(!isNaN(dataSet.bots)){
+            let spam = prompt(`ChatSpam would spam your google meets chat with whatever phrase you would like. Would you like to activate ChatSpam? If yes please write yes,if no write nothing and continue...`)
+            if(spam.trim().length>0){
+                if(spam.toLowerCase().includes("yes")){
+                    let phrase = prompt("What phrase would you like to be spammed?")
+                    if(phrase.trim().length==0){
+                        alert("Please refresh page and try again. You didn't write down a phrase....")
                     }
                     else{
-                        alert("alright, give it another go...😊")
+                        dataSet.phrase=phrase
+                        dataSet.spam="Active"
                     }
                 }
             }
-
-        })}
-    else{
-        setInterval(function(){
-            if(document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0]!==undefined){
-                document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0].click();
-            }
-            if(document.getElementsByClassName("fwk7ze")[0]!==undefined){
-                if(document.getElementsByClassName("fwk7ze")[0].innerText.includes("refresh")){
-                    location.reload();
+            let check = confirm(`Here's your bot request:\nLink: ${dataSet.link}\nBot-Count: ${dataSet.bots}\nChatSpam: ${dataSet.spam}\nPhrase: ${dataSet.phrase}\n\nPlease click ok to start botting!!`)
+            if(check){
+                let spam=false;
+                if(dataSet.spam=="Active"){
+                    spam=true;
+                }
+                for(let i=0;i<dataSet.bots;i++){
+                    window.open(dataSet.link+`?botting=true&spam=${spam}&phrase=${dataSet.phrase}&#BOT_${i+1}`);
                 }
             }
-            if(document.getElementsByClassName("CRFCdf")[0]!==undefined){
-                location.reload();
-            }
-            if(document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0]!==undefined){
-                document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0].click();
-            }
+        }
+        else{
+            alert("Please refresh page and try again. You didn't write down a number....")
+        }
+    }
+})
 
-            let phrase=localStorage.getItem("spamMsg");
-            botSay(phrase)
-        },1000)
+let urlParams = new URLSearchParams(window.location.search);
+if(urlParams.get("botting")=="true"){
+    setInterval(function(){
+        enter();
+    },1000)
+    if(urlParams.get("spam")=="true"){
+        if(urlParams.get("phrase")!=="none"){
+            botSay(urlParams.get("phrase"),1000)
+        }
     }
 }
 
-function botSay(phrase){
-    document.getElementsByClassName("uArJ5e UQuaGc kCyAyd QU4Gid foXzLb ")[1].click();
-    document.getElementsByClassName("KHxj8b tL9Q4c")[0].value=phrase;
-    let send=document.getElementsByClassName("uArJ5e Y5FYJe cjq2Db IOMpW Cs0vCd RDPZE")[0];
-    send.setAttribute("aria-disabled",false);
-    send.click();
+function botSay(phrase,wait){
+    setInterval(function(){
+        document.getElementsByClassName("uArJ5e UQuaGc kCyAyd QU4Gid foXzLb ")[1].click();
+        document.getElementsByClassName("KHxj8b tL9Q4c")[0].value=phrase;
+        let send=document.getElementsByClassName("uArJ5e Y5FYJe cjq2Db IOMpW Cs0vCd RDPZE")[0];
+        send.setAttribute("aria-disabled",false);
+        send.click();
+    },wait)
 }
 
+function enter(){
+    if(document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0]!==undefined){
+        document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0].click();
+    }
+    if(document.getElementsByClassName("fwk7ze")[0]!==undefined){
+        if(document.getElementsByClassName("fwk7ze")[0].innerText.includes("refresh")){
+            location.reload();
+        }
+    }
+    if(document.getElementsByClassName("CRFCdf")[0]!==undefined){
+        location.reload();
+    }
+    if(document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0]!==undefined){
+        document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0].click();
+    }
+}
 
