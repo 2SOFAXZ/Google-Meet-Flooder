d88e8af2e54e88df49e00682a5e7d1872505aae0
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2021-05-26 17:32:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,49 +9,6 @@
 // @grant        none
 // ==/UserScript==
 
-window.addEventListener("keyup",function(evt){
-    let dataSet = {"link":"","bots":"","spam":"Inactive","phrase":"none"}
-    if(evt.ctrlKey&&evt.altKey&&evt.keyCode==66){
-        dataSet.link=location.href;
-        dataSet.bots = prompt(`How many bots would you like to spam ${dataSet.link}? Recommended Max : 15 or your browser may crash ☠`)
-        if(!isNaN(dataSet.bots)){
-            let spam = prompt(`ChatSpam would spam your google meets chat with whatever phrase you would like. Would you like to activate ChatSpam? If yes please write yes,if no write nothing and continue...`)
-            if(spam.trim().length>0){
-                if(spam.toLowerCase().includes("yes")){
-                    let phrase = prompt("What phrase would you like to be spammed?")
-                    if(phrase.trim().length==0){
-                        alert("Please refresh page and try again. You didn't write down a phrase....")
-                    }
-                    else{
-                        dataSet.phrase=phrase
-                        dataSet.spam="Active"
-                    }
-                }
-            }
-            let check = confirm(`Here's your bot request:\nLink: ${dataSet.link}\nBot-Count: ${dataSet.bots}\nChatSpam: ${dataSet.spam}\nPhrase: ${dataSet.phrase}\n\nPlease click ok to start botting!!`)
-            if(check){
-                let spam=false;
-                if(dataSet.spam=="Active"){
-                    spam=true;
-                }
-                let prm;
-                for(let i=0;i<dataSet.bots;i++){
-                    if(dataSet.link.includes("?")){
-                        prm = "&botting"
-                    }
-                    else{
-                        prm = "?botting"
-                    }
-                    window.open(dataSet.link+`${prm}=true&spam=${spam}&phrase=${dataSet.phrase}&#BOT_${i+1}`);
-                }
-            }
-        }
-        else{
-            alert("Please refresh page and try again. You didn't write down a number....")
-        }
-    }
-})
-
 let urlParams = new URLSearchParams(window.location.search);
 if(urlParams.get("botting")=="true"){
     setInterval(function(){
